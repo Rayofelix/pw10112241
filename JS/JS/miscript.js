26a101c39388d6a0c4f33095f8f77523ce7b5743
Rayo <119818831+Rayofelix@users.noreply.github.com>
2024-04-17 10:48:47 -0700

    Introduccion a JS

---
+++ JS/JS/miscript.js
@@ -0,0 +1,10 @@
+var a = 0;
+let b = 0; //scope - alcance - ambito
+const c = 3.1416;
+alert("JS in process");
+function button(){
+    alert("Click")
+}
+const arrowFunction = () =>{
+    alert("ArrowFunction")
+}
