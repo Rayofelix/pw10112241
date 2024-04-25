0b005678150bade5fcbcac7eee26a904d82a81ce
Rayo <119818831+Rayofelix@users.noreply.github.com>
2024-04-25 10:49:47 -0700

    Conexion a base de datos

--- proyecto/backend/app.js
+++ proyecto/backend/app.js
@@ -10,9 +10,39 @@ let app = express();
 app.use(express.json());
 app.use(cors());
 
+
+// conexion a sql
+let conexion = mysql.createConnection({
+    host:'127.0.0.1',
+    password:'',
+    user:'root',
+    database:'pwdata',
+    port:'3306'
+});
+
+// nos conectamos a sql
+conexion.connect(function(error){
+    if(error){
+        throw error;
+    }else{
+        console.log('Conectado a la base de datos')
+    }
+});
+
 // Definir rutas de acceso
 app.get('/',function(req,res){
-    res.send('Rutassssss de iniciosssssss');
+    res.send('Ruta de inicio');
+});
+
+// seleccionar todos los clientes
+app.get('/api/clients',(req,res)=>{
+    conexion.query('select * from clients',(error,rows)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(rows);
+        }
+    });
 });
 
 // Tercer paso
