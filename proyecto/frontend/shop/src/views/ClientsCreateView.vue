69fadd0cabf8df5a0a984a5413329f723822fedd
Rayo <119818831+Rayofelix@users.noreply.github.com>
2024-05-16 10:52:22 -0700

    Add files via upload

--- proyecto/frontend/shop/src/views/ClientsCreateView.vue
+++ proyecto/frontend/shop/src/views/ClientsCreateView.vue
@@ -3,6 +3,9 @@
         <div class="card">
             <div class="card-header">
                 <h4>Add Client</h4>
+                <div v-if="message==1" class=" alert alert-success" role="alert" >
+                    Data Save Succesfully!
+                </div>
             </div>
             <div class="card-body">
                 <div class="mb-3">
@@ -38,17 +41,20 @@
                     <input type="text" class="form-control" v-model="model.client.cp">
                 </div>
                 <div class="mb-3">
-                    <button class="btn btn-primary">Sent</button>
+                    <button class="btn btn-primary" @click="createClient()">Sent</button>
                 </div>
             </div>
         </div>
     </div>
 </template>
 <script>
+import axios from 'axios';
+
 export default{
     name: 'ClientsCreate',
     data(){
         return{
+            message: 0,
             model:{
                 client:{
                     id:'',
@@ -65,7 +71,21 @@ export default{
     },
     methods:{
         createClient(){
-            
+            axios.post('http://localhost:3000/api/clients',this.model.client).then(res =>{
+                if(res.data.affectedRows == 1){
+                    this.model.client = { //Limpiar cuadros de texto al dar click al boton
+                        id:'',
+                        name:'',
+                        lastname:'',
+                        address:'',
+                        phone:'',
+                        rfc:'',
+                        curp:'',
+                        cp:''
+                    }
+                    this.message = 1;
+                }
+            });
         }
     }
 }
